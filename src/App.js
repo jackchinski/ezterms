ffc5ddac30297bffa6cbdfe232dfbaf435a3763f
Syed Hassan <sai-yed@outlook.com>
2024-03-05 12:39:53 -0500

    refactor: test 02, fetching test sample data

--- src/App.js
+++ src/App.js
@@ -1,47 +1,37 @@
 import React, { useState, useEffect } from "react";
-import colRef from "./firebase.config";
-import { getDocs } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase.config";
+
+async function fetchDataFirestore() {
+  const querySnapshot = await getDocs(collection(db, "test"));
+  const data = [];
+  querySnapshot.forEach((doc) => {
+    data.push({ id: doc.id, ...doc.data() });
+  });
+  return data;
+}
 
 function App() {
-  const [data, setData] = useState(null);
+  const [userData, setUserData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const snapshot = await getDocs(colRef);
-        const dataArray = snapshot.docs.map(doc => {
-          return { id: doc.id, ...doc.data() };
-        });
-        setData(dataArray);
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-    };
-
+    async function fetchData() {
+      const data = await fetchDataFirestore();
+      setUserData(data);
+    }
     fetchData();
   }, []);
 
   return (
     <div>
       <h1>Data from Firebase Firestore</h1>
-      {data ? (
-        <ul>
-          {data.map(item => (
-            <li key={item.id}>
-              <strong>Name:</strong> {item.Name}, <strong>ID:</strong> {item.id}
-              <br />
-              <strong>Flags:</strong> 
-              <ul>
-                <li>Safe: {item.Flags.Safe.join(", ")}</li>
-                <li>Caution: {item.Flags.Caution.join(", ")}</li>
-                <li>Danger: {item.Flags.Danger.join(", ")}</li>
-              </ul>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
+      {userData.map((user) => (
+        <div key={user.id}>
+          <p>{user.name}</p>
+          <p>{user.age}</p>
+          <p>{user.bio}</p>
+        </div>
+      ))}
     </div>
   );
 }
