dfa6494323830bc4f34a1980200d7f061e43f1df
jackchinski <marcin.jacz@gmail.com>
2024-03-05 11:28:26 -0500

    useEffect so that the testing functions only run once

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
+import React, { useEffect } from 'react'; 
 import logo from './logo.svg';
 import './App.css';
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -23,29 +18,27 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 
-
-// do all of this in a useEffect so it only runs once 
-const docRef = await addDoc(collection(db, "users"), {
-  first: "Ada", 
-  last: "Lovelace", 
-  born: 1815
-}); 
-
-console.log("Document writting with ID: ", docRef.id)
-
-const docRef2 = await addDoc(collection(db, "users"), {
-  first: "Marcin", 
-  last: "Jaczynski", 
-  born: 2000
-}); 
-
-console.log("Document writting with ID: ", docRef2.id)
-
-
-
 function App() {
-  return (
-    <div className="App">
+  useEffect(() => {
+    (async () => {
+      try {
+        const docRef = await addDoc(collection(db, "users"), {
+          first: "Ada", 
+          last: "Lovelace", 
+          born: 1815
+        }); 
+
+        console.log("Document writting with ID: ", docRef.id);
+
+      } catch (e) {
+        console.log("Error adding document", e);
+      }
+    })();
+  }, []);
+  
+  
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -56,12 +49,12 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          EAT ASS
+          >
+          HELLO THERE EZ TERMS USERS
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
