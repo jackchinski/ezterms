38f762b5f060a557c0666e9401c4effb82eb01ce
jackchinski <marcin.jacz@gmail.com>
2024-03-08 13:19:33 -0500

    reading from db

--- src/testingFunctions/addToDB.js
+++ src/testingFunctions/addToDB.js
@@ -1,4 +1,4 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
 async function addUserToFirebase(db) {
   try {
@@ -15,4 +15,21 @@ async function addUserToFirebase(db) {
   }
 }
 
-export { addUserToFirebase };
+async function readUserFromFirebase(db) {
+  try {
+    const querySnapshot = await getDocs(collection(db, "sortedTerms"));
+    console.log("====DOCUMENT RETRIEVED FROM DB====");
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+
+
+    //
+      
+    //
+  } catch (e) {
+    console.log("error: ", e);
+  }
+}
+
+export { addUserToFirebase, readUserFromFirebase };
