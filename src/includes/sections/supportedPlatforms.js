47b1088fb57b9f5dcd720db49b3ed7ad52cd195b
Syed Hassan <sai-yed@outlook.com>
2024-03-21 00:35:44 -0400

    feat: added supported platform grid with icons

--- src/includes/sections/supportedPlatforms.js
+++ src/includes/sections/supportedPlatforms.js
@@ -1,18 +1,47 @@
 import React from "react";
+import Amazon from "../assets/icons/Amazon.png";
+import BeReal from "../assets/icons/BeReal.png";
+import Etsy from "../assets/icons/Etsy.png";
+import Instagram from "../assets/icons/Instagram.png";
+import LinkedIn from "../assets/icons/LinkedIn.png";
+import Pinterest from "../assets/icons/Pinterest.png";
+import Shopify from "../assets/icons/Shopify.png";
+import Snapchat from "../assets/icons/Snapchat.png";
+import Telegram from "../assets/icons/Telegram.png";
+import Ticktok from "../assets/icons/Ticktok.png";
+import Wix from "../assets/icons/Wix.png";
+import X from "../assets/icons/X.png";
+
+import "../css/home.css";
 
 const SupportedPlatforms = () => {
+  const platformImages = {
+    Amazon,
+    BeReal,
+    Etsy,
+    Instagram,
+    LinkedIn,
+    Pinterest,
+    Shopify,
+    Snapchat,
+    Telegram,
+    Ticktok,
+    Wix,
+    X,
+  };
+
   return (
     <>
       <section id="SupportedPlatforms">
         <h1>Supported Platforms</h1>
-        <p>
-          This is the homepage of our website. Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Sed sit amet urna a odio luctus suscipit.
-          Nullam lacinia ligula nec est sodales, nec vehicula orci lacinia.
-        </p>
-        <p>
-          Feel free to explore our services, learn more about us, or contact us!
-        </p>
+        <div className="platform-grid">
+          {Object.entries(platformImages).map(([platform, imageSrc], index) => (
+            <div className="grid-item" key={index}>
+              <img src={imageSrc} alt={platform} />
+              <h5>{platform}</h5>
+            </div>
+          ))}
+        </div>
       </section>
     </>
   );
