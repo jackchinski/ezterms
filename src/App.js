be1ea83cfedba7bc8634cda3ad86ce42b9dc5754
Syed Hassan <sai-yed@outlook.com>
2024-04-02 22:12:12 -0400

    feat: rendering page data

--- src/App.js
+++ src/App.js
@@ -11,11 +11,7 @@ import PasteTOS from './includes/pages/PasteTOS';
 import Request from './includes/pages/Request';
 import NoPage from './includes/pages/NoPage';
 
-import data from
-
-// import Schema01 from "./includes/Schema01";
-// import Schema02 from "./includes/Schema02";
-// import Schema03 from "./includes/Schema03";
+import Page0 from "./includes/pages/terms/Page0";
 
 function App() {
   return (
@@ -30,21 +26,10 @@ function App() {
           <Route path="*" element={<NoPage />} />
         </Routes>
       
-      {/* <h1>Data from Firebase Firestore</h1>
-      <h2>Collection: sortedTerms</h2>
-      <hr/>
 
-      <h3>Schema 01</h3>
-      <Schema01 />
+      <h3>Page 0</h3>
+      <Page0 />
       <hr/>
-
-      <h3>Schema 02</h3>
-      <Schema02 />
-      <hr/>
-
-      <h3>Schema 03</h3>
-      <Schema03 />
-      <hr/> */}
 
       </div>
     </BrowserRouter>
---
+++ src/includes/pages/terms/Page0.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { backendTerms } from '../../../data/backendTerms';
+
+function Page0() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await backendTerms();
+        setUserData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (!userData) {
+    // Data is not yet fetched, return a loading indicator or null
+    return null;
+  }
+
+  return (
+    <div>
+      <h2>{userData.sectionHeading}</h2>
+      <p>Description: {userData.description}</p>
+      <p>Update Info: {userData.updateInfo}</p>
+      <h3>Danger</h3>
+      <ul>
+        {userData.Danger.map((dangerItem, index) => (
+          <li key={index}>
+            <p>Summary: {dangerItem.Summary}</p>
+            <p>Clause Section: {dangerItem.ClauseSection}</p>
+            <p>Infringement Keyword: {dangerItem.InfringementKeyword}</p>
+          </li>
+        ))}
+      </ul>
+      <h3>Safe</h3>
+      <ul>
+        {userData.Safe.map((safeItem, index) => (
+          <li key={index}>
+            <p>Summary: {safeItem.Summary}</p>
+            <p>Clause Section: {safeItem.ClauseSection}</p>
+            <p>Infringement Keyword: {safeItem.InfringementKeyword}</p>
+          </li>
+        ))}
+      </ul>
+      <h3>Caution</h3>
+      <ul>
+        {userData.Caution.map((cautionItem, index) => (
+          <li key={index}>
+            <p>Summary: {cautionItem.Summary}</p>
+            <p>Clause Section: {cautionItem.ClauseSection}</p>
+            <p>Infringement Keyword: {cautionItem.InfringementKeyword}</p>
+          </li>
+        ))}
+      </ul>
+      <img src={userData.assetRef} alt="Asset" />
+    </div>
+  );
+}
+
+export default Page0;
