32f8cbd16f7c27c1fa691853f7ab829b1b202d8e
Syed Hassan <sai-yed@outlook.com>
2024-04-09 16:38:09 -0400

    feat: updated FB routes with some other changes

--- src/includes/pages/terms/termsPage.js
+++ src/includes/pages/terms/termsPage.js
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import { backendTerms } from "../../../data/backendTerms";
 import NoPage from "../../../includes/pages/NoPage";
 
+import { AiOutlineWarning, AiOutlineSafety } from "react-icons/ai";
+import { LuConstruction } from "react-icons/lu";
+
 import "../../css/termsPage.css";
 
 function TermsPage() {
@@ -45,57 +48,76 @@ function TermsPage() {
     return str.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase());
   };
 
+  const getKeyIcon = (key) => {
+    switch (key) {
+      case "Danger":
+        return <AiOutlineWarning />;
+      case "Caution":
+        return <LuConstruction />;
+      case "Safe":
+        return <AiOutlineSafety />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
-      <section id="terms-hero">
-        <div className="inner-container flex-container">
-          {currentPage.assetRef ? (
-            <div className="flex-item">
-              <img
-                src={`${process.env.PUBLIC_URL}/${currentPage.assetRef}`}
-                alt={`${currentPage.assetRef} Icon`}
-              />
-            </div>
-          ) : (
-            <p>Your icon asset reference does not exist</p>
-          )}
-          <div className="flex-item">
-            <p>
-              {currentPage.description || "Page description does not exist"}
-            </p>
-            <p>{currentPage.updateInfo || "Page update Info does not exist"}</p>
-          </div>
+  <section id="terms-hero">
+    <div className="inner-container flex-container">
+      {currentPage.assetRef ? (
+        <div className="flex-item">
+          <img
+            src={`${process.env.PUBLIC_URL}/${currentPage.assetRef}`}
+            alt={`${currentPage.assetRef} Icon`}
+          />
         </div>
-      </section>
-      <div className="inner-container">
-        <div>
-          {pageObjectKeys.map((key, index) => (
-            <div key={index} className={key}>
-              <h3>
-                {key} ({currentPage[key].length})
-              </h3>
-              {currentPage[key].map((item, itemIndex) => (
-                <div key={itemIndex} className="clause-container">
-                  <p><strong>{itemIndex + 1}</strong></p>
-                  <div className="clause-box">
-                  <div className="clause-header">
-                    <p>
-                      <strong>{toTitleCase(item.InfringementKeyword)}</strong>
-                    </p>
-                    <p>
-                      Found in:{" "}
-                      <strong>{toTitleCase(item.ClauseSection)}</strong>
-                    </p>
-                  </div>
-                  <p>{item.Summary}</p>
-                  </div>
+      ) : (
+        <p>Your icon asset reference does not exist</p>
+      )}
+      <div className="flex-item">
+        <p>
+          {currentPage.description || "Page description does not exist"}
+        </p>
+        <p>{currentPage.updateInfo || "Page update Info does not exist"}</p>
+      </div>
+    </div>
+  </section>
+  <div className="inner-container">
+    <div>
+      {pageObjectKeys.map((key, index) => (
+        <div key={index} className={key}>
+          <div className="clause-object-heading">
+          <div className="clause-icon">{getKeyIcon(key)}</div>
+          <h3>
+           {key} ({currentPage[key].length})
+          </h3>
+          </div>
+          {currentPage[key].map((item, itemIndex) => (
+            <div key={itemIndex} className="clause-container">
+              <p className="clause-count">
+                {itemIndex + 1}
+              </p>
+              <div className="clause-box">
+                <div className="clause-header">
+                  <p>
+                    <strong>{toTitleCase(item.InfringementKeyword)}</strong>
+                  </p>
+                  <p>
+                    Found in:{" "}
+                    <strong>{toTitleCase(item.ClauseSection)}</strong>
+                  </p>
                 </div>
-              ))}
+                <p>{item.Summary}</p>
+              </div>
             </div>
           ))}
         </div>
-      </div>
-    </>
+      ))}
+    </div>
+  </div>
+</>
+
   );
 }
 
